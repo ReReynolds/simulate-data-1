bc7ae368e6c24ce82b99868d8f574a4cb7018c2c
Renee Reynolds <reynold1@arizona.edu>
2024-06-25 18:32:25 -0500

    Add testing note

--- simualte_plot_data.R
+++ simualte_plot_data.R
@@ -4,3 +4,14 @@
 # 25 JUNE 2024
 
 
+# Simulate predictor variable
+x <- rnorm(n = 100)
+
+# Simulate response variable with some noise
+y <- 0.5 * x + rnorm(n = 100, sd = 0.2)
+
+#Plot the data 
+plot(x = x, y = y)
+
+#Making this change to test Git push/pull
+
